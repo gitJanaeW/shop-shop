3ca420ed923d140152f2bf85beef10afa50ebe8a
janae.wel <janae.wel@gmail.com>
2022-10-19 12:33:58 -0400

    fixed bug

---
+++ client/build/precache-manifest.2a9219c143b999187a0101a601e8a821.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6635ce9634976cad40ae99bcfa235815",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d655519ae4650bde0df",
+    "url": "/static/css/main.db415adc.chunk.css"
+  },
+  {
+    "revision": "a6df5ec034a6b678e564",
+    "url": "/static/js/2.17d7b70a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.17d7b70a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d655519ae4650bde0df",
+    "url": "/static/js/main.e1b788ef.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.e1b788ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d52f44d6fb82598fed4",
+    "url": "/static/js/runtime-main.c62b8046.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
